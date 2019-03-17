0f93d5a5bf86e92fc79f894c7ba89606c8744ef5
TinyBuster1 <34807329+TinyBuster1@users.noreply.github.com>
2019-03-17 18:59:33 +0200

    Update GUIandDBCommunication.py

--- GUIandDBCommunication.py
+++ GUIandDBCommunication.py
@@ -27,6 +27,58 @@ class GUIandDB:
                                                MentorList["MentorLastName"].get(),MentorList["MentorPhone"].get())
         LogicGui.LogicGui.returnToMainFromMentor(self, top)
         
+            @staticmethod
+    def Logged(self, LoginList, top):
+        
+        userType = BlinkyDataBaseManagment.userChecker(LoginList)
+        
+        if userType == 1:
+            # here do the transfer to admin page
+
+            LogicGui.LogicGui.OpenAdminPanelWin(self, top)
+            print("Admin login success!")
+        elif userType == 2:
+            # here do the transfer to mentor page
+            LogicGui.LogicGui.OpenMentorPanelWin(self, top)
+            print("Mentor login success!")
+        elif userType == 3:
+            
+            PicAndPharases = {}
+            PicAndPharases["pic1button"] = BlinkyDataBaseManagment.takePic(LoginList["UserNameText"].get(), "1")
+            PicAndPharases["pic2button"] = BlinkyDataBaseManagment.takePic(LoginList["UserNameText"].get(), "2")
+            PicAndPharases["pic3button"] = BlinkyDataBaseManagment.takePic(LoginList["UserNameText"].get(), "3")
+            PicAndPharases["pic4button"] = BlinkyDataBaseManagment.takePic(LoginList["UserNameText"].get(), "4")
+
+            PicAndPharases["Phrase1Button"] = BlinkyDataBaseManagment.takePhrase(LoginList["UserNameText"].get(), "1")
+            PicAndPharases["Phrase2Button"] = BlinkyDataBaseManagment.takePhrase(LoginList["UserNameText"].get(), "2")
+            PicAndPharases["Phrase3Button"] = BlinkyDataBaseManagment.takePhrase(LoginList["UserNameText"].get(), "3")
+            PicAndPharases["Phrase4Button"] = BlinkyDataBaseManagment.takePhrase(LoginList["UserNameText"].get(), "4")
+
+            LogicGui.LogicGui.OpenUserPanelWin(self,LoginList["UserNameText"].get(), PicAndPharases, top)
+            print("User login success!")
+        else:
+            print("wrong id or password please try again!")
+
+
+    @staticmethod
+    def RefreshUser(ChangeList,top):
+        role = ChangeList["picIDcomboBox"].get()
+
+        button = ChangeList["pic"+role+"button"]
+
+        prog_call = sys.argv[0]
+        prog_location = os.path.split(prog_call)[0]
+        photo_location = os.path.join(prog_location, ChangeList["path"])
+        image = tk.PhotoImage(file=photo_location)
+
+        button.configure(image=image)
+        button.configure(pady="0")
+        button.update()
+
+        ChangeList["LoginFrame"].update()
+
+        top.mainloop()
+        
 
     
     
