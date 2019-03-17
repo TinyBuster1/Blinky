48f8b21b00532a19975cf561b6558234b46f33f7
TinyBuster1 <34807329+TinyBuster1@users.noreply.github.com>
2019-03-17 18:57:41 +0200

    Add files via upload

---
+++ LogicGui.py
@@ -0,0 +1,89 @@
+import GUI
+import NewUserRegister
+import RegisterNewMentor
+import Infohelp
+import Tkinter as tk
+import FeedbackFrame
+import UserPanel
+import MentorPanel
+import AdminPanel
+
+class LogicGui:
+    @staticmethod
+    def returnToMainFromUser(self,top):
+        self.PatientRegisterFrame.destroy()
+        tk.Frame(GUI.vpReturnToMain(top))
+        
+    @staticmethod
+    def returnToMainFromMentor(self,top):
+        self.RegisterNewMentorFrame.destroy()
+        tk.Frame(GUI.vpReturnToMain(top))
+        
+    @staticmethod  
+    def RegNewMenWin(self,top):
+        self.MainPageFrame.destroy()
+        tk.Frame(RegisterNewMentor.RegisterNewMentorLevel().create_RegMentor(top))
+
+    @staticmethod
+    def RegNewUserWin(self, top):
+        self.MainPageFrame.destroy()
+        tk.Frame(NewUserRegister.UserRegister().Create_RegNewUSer(top))
+        
+    @staticmethod     
+    def ShowInfo(self, top):
+        self.MainPageFrame.destroy()
+        tk.Frame(Infohelp.InfoFrame().create_InfoWin(top))
+        
+    @staticmethod
+    def InfoBackToMain(self,top):
+        self.Frame1.destroy()
+        tk.Frame(GUI.vpReturnToMain(top))
+        
+    @staticmethod
+    def FeedBackWin(self,top):
+        self.Frame1.destroy()
+        tk.Frame(FeedbackFrame.FeedBackFrame().create_FeedBackWin(top))
+        
+    @staticmethod
+    def BackToInfoHelp(self,top):
+        self.Feedback.destroy()
+        tk.Frame(Infohelp.InfoFrame().create_InfoWin(top))
+        
+    @staticmethod
+    def OpenUserPanelWin(self,UserID,PicAndPharases, top):
+        self.MainPageFrame.destroy()
+        tk.Frame(UserPanel.UserPanel().create_UserPanelWin(UserID,PicAndPharases,top))
+
+    @staticmethod
+    def OpenMentorPanelWin(self, top):
+        self.MainPageFrame.destroy()
+        tk.Frame(MentorPanel.MentorPanel().create_MentorPanelWin(top))
+
+    @staticmethod
+    def OpenAdminPanelWin(self, top):
+        self.MainPageFrame.destroy()
+        tk.Frame(AdminPanel.AdminPanel().create_AdminPanelWin(top))
+
+    @staticmethod
+    def LogoutfromUser(self, top):
+        self.LoginFrame.destroy()
+        tk.Frame(GUI.vpReturnToMain(top))
+
+    @staticmethod
+    def LogoutfromMentor(self, top):
+        self.MentorCntPnl.destroy()
+        tk.Frame(GUI.vpReturnToMain(top))
+
+    @staticmethod
+    def LogoutfromAdmin(self, top):
+        self.AdminOptions.destroy()
+        tk.Frame(GUI.vpReturnToMain(top))
+
+        
+        
+        
+        
+        
+        
+        
+        
