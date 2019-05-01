37036b0d3f976291da4bb943abf0959654d86551
amit <34807427+1amitos1@users.noreply.github.com>
2019-05-01 21:22:12 +0300

    Add files via upload

--- Forgot_password.py
+++ Forgot_password.py
@@ -61,7 +61,57 @@ def Forgot_password_U():
 
     root.mainloop()
 
+def Forgot_password_M(MentorID):
+    root = tk.Tk()
+    root.title("Forgot Password")
+    root.geometry("500x500")
 
+
+    UIDname_l=tk.Label(root, text="ENTER USER ID:").pack()
+    Unentry=tk.Entry(root,)
+    Unentry.pack()
+
+
+    #temp=UserComboBox.get()
+
+    password_l=tk.Label(root, text="ENTER NEW PASSWORD:").pack()
+    password_entry=tk.Entry(root)
+    password_entry.pack()
+
+    def test(password_entry,Unentry,MentorID):
+        new_password = password_entry.get()
+        user_id= Unentry.get()
+
+
+        conn = pyodbc.connect(BlinkyDataBaseManagment.mySQLserver)
+
+        cursor = conn.cursor()
+        sql=''' UPDATE BlinkyDB.dbo.[User]
+                SET [User].password=(?)
+                WHERE [User].uid=? AND [User].mid=?;'''
+        cursor.execute(sql,new_password,user_id,MentorID)
+        conn.commit()
+
+        sql='''SELECT password FROM BlinkyDB.dbo.[User]
+                WHERE [User].uid=? AND [User].mid=?;'''
+        cursor.execute(sql,user_id, MentorID)
+        row = cursor.fetchone()
+
+        if(row!=None):
+            password=str(row)
+            messagebox.showinfo("REST PASSWORD", "Your password is"+password)
+        else:
+            messagebox.showinfo("REST PASSWORD", "A problem has occurred")
+
+
+
+    action_with_args = partial(test, password_entry,Unentry,MentorID)
+    B_recover_password=tk.Button(root,text="REST PASWORD", command= action_with_args).pack()
+
+
+
+
+    root.mainloop()
 
 
 
