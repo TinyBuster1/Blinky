7a87b98f12730666775a51c1380f70f60a34b9eb
TinyBuster1 <34807329+TinyBuster1@users.noreply.github.com>
2019-05-19 13:16:55 +0300

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,10 +1,10 @@
 import datetime
 
-
+myCommit = "test"
 now = datetime.datetime.now()
 time =  now.day.__str__() + "/" + now.month.__str__() + "/" + now.year.__str__() + " at: " + now.time().__str__()
 
-def print_time(time):
-    print("Latest commit is done at: " + time)
+def print_time(time,myCommit):
+    print("Latest commit is done at: " + time + ", commit: " + myCommit)
 
-print_time(time)
+print_time(time,myCommit)
