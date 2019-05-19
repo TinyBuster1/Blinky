807ff7f735f46560b2a3b322b9ff81f359ed94b1
TinyBuster1 <34807329+TinyBuster1@users.noreply.github.com>
2019-05-19 15:05:35 +0300

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,4 +1,7 @@
 import datetime
+import smtplib
+
+#### 1 Everyone Commits To the Mainline Every Day ####
 
 myCommit = "test"
 now = datetime.datetime.now()
@@ -7,4 +10,21 @@ time =  now.day.__str__() + "/" + now.month.__str__() + "/" + now.year.__str__()
 def print_time(time,myCommit):
     print("Latest commit is done at: " + time + ", commit: " + myCommit)
 
-print_time(time,myCommit)
+print_time(time, myCommit)
+
+
+#### 2  Everyone can see what's happening ####
+
+emailList = ["alexabo4@ac.sce.ac.il"]
+def send_Mails(emailList,time):
+    for mail in emailList:
+        if '@' not in mail:
+            return False
+        s = smtplib.SMTP('smtp.gmail.com', 587)
+        s.starttls()
+        s.login("blinkysendmsg@gmail.com", "sukablyat123")
+
+        message = 'Subject: {}\n\n{}'.format('new code commit! at',time)
+        s.sendmail("blinkysendmsg@gmail.com", mail, message)
+        s.quit()
+send_Mails(emailList,time)
