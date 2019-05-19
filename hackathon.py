ec7b37a5b8afe89b0d9b28a1e8fe832b61acfb7b
TinyBuster1 <34807329+TinyBuster1@users.noreply.github.com>
2019-05-19 13:10:12 +0300

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,12 +1,12 @@
-import tkinter as tk
+
 import unittest
 import datetime
 
 class Hackathon(unittest.TestCase):
     now = datetime.datetime.now()
-    time  = + now.day.__str__() + "/" + now.month.__str__() + "/" + now.year.__str__() + " at: " + now.time().__str__()
+    time =  now.day.__str__() + "/" + now.month.__str__() + "/" + now.year.__str__() + " at: " + now.time().__str__()
 
-    def print(self):
+    def print_time(self):
         print("Latest commit is done at: " + self.time)
 
 if __name__ == '__main__':
