5ad1aec6df24fa37bb02d6e3f6f9e60a4190466e
TinyBuster1 <34807329+TinyBuster1@users.noreply.github.com>
2019-05-19 12:04:53 +0300

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,2 +1,14 @@
 
-print("Test")
+import tkinter as tk
+import unittest
+
+
+class Hackathon(unittest.TestCase):
+
+    roottest = tk.Tk()
+    testLabel = tk.Label()
+    testLabel1 = tk.Label()
+    
+    def test_label(self):
+        print('testing label')
+        self.assertNotEqual(self.testLabel1, self.testLabel, 'not equal')
