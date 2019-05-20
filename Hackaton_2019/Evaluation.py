e66a20bb16db68bacccddd0547b1316ba0d9a3f3
Chopper420 <34807689+Chopper420@users.noreply.github.com>
2019-05-20 10:03:47 +0300

    Add files via upload

---
+++ Hackaton_2019/Evaluation.py
@@ -0,0 +1,36 @@
+import datetime
+import smtplib
+import time
+import sys
+import os
+
+
+### help GUI function ###
+
+def GUI():
+    print(" calling GUI window , testing benchmark.")
+	
+	
+### 4 Evaluation and vision ###
+precent = 1.1
+def Evaluation(precent):
+    prog_call = sys.argv[0]
+    prog_location = os.path.split(prog_call)[0]
+    reports = prog_location + "\Reports" + "\\" + "Evaluation.txt"
+    with open(reports, "r") as f:
+        eval = f.read()
+        print(eval)
+    start_time = time.time()
+    GUI()
+    endTime = now.time()
+    total = time.time() - start_time
+    print(total*precent)
+    print(eval)
+    if total*precent > float(eval):
+        print("The program is running to slow compared to the last build.")
+    f.close()
+    with open(reports, "w") as f:
+        f.write(str(total))
+        f.close()
+
+Evaluation(precent)
