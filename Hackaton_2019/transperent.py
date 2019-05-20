f3c712b7c52821ad5d200f29b2b0f416a9383bea
genadyol <34807328+genadyol@users.noreply.github.com>
2019-05-20 09:56:37 +0300

    Update transperent.py

--- Hackaton_2019/transperent.py
+++ Hackaton_2019/transperent.py
@@ -5,6 +5,9 @@ import sys
 import os
 
 #### 2  Everyone can see what's happening ####
+myCommit = "test"
+now = datetime.datetime.now()
+time1 =  now.day.__str__() + "/" + now.month.__str__() + "/" + now.year.__str__() + " at: " + now.time().__str__()
 
 emailList = ["alexabo4@ac.sce.ac.il"]
 def send_Mails(emailList,time1):
