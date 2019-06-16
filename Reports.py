6b18c18501b0592be0bff55ce54ace9bc0d195bd
amit <34807427+1amitos1@users.noreply.github.com>
2019-06-16 22:22:19 +0300

    Add files via upload

--- Reports.py
+++ Reports.py
@@ -1,4 +1,5 @@
 #import Tkinter as tk
+from time import localtime
 
 try:
     import Tkinter as tk
@@ -61,20 +62,20 @@ def statisticsReport():
     tk.Label(root, text="Deleted:").pack()
     with open(reports, "r") as f:
         tk.Label(root, text=f.read()).pack()
-    allImages = BlinkyDataBaseManagment.getAllImages()
-    allTitles = BlinkyDataBaseManagment.getAllTitles()
-    tk.Label(root, text="----------------").pack()
-    tk.Label(root, text="Images:").pack()
-    tk.Label(root, text=allImages).pack()
-    tk.Label(root, text="total:").pack()
-    tk.Label(root, text=len(allImages)).pack()
-    tk.Label(root, text="----------------").pack()
-    tk.Label(root, text="Phrases:").pack()
-    for phrase in allTitles:
-        tk.Label(root, text=phrase).pack()
-    tk.Label(root, text="total:").pack()
-    tk.Label(root, text=len(allTitles)).pack()
-    tk.Label(root, text="----------------").pack()
+    # allImages = BlinkyDataBaseManagment.getAllImages()
+    # allTitles = BlinkyDataBaseManagment.getAllTitles()
+    # tk.Label(root, text="----------------").pack()
+    # tk.Label(root, text="Images:").pack()
+    # tk.Label(root, text=allImages).pack()
+    # tk.Label(root, text="total:").pack()
+    # tk.Label(root, text=len(allImages)).pack()
+    # tk.Label(root, text="----------------").pack()
+    # tk.Label(root, text="Phrases:").pack()
+    # for phrase in allTitles:
+    #     tk.Label(root, text=phrase).pack()
+    # tk.Label(root, text="total:").pack()
+    # tk.Label(root, text=len(allTitles)).pack()
+    # tk.Label(root, text="----------------").pack()
     root.mainloop()
     f.close()
 
@@ -104,3 +105,136 @@ def onlineUsers():
     tk.Label(root, text="----------------").pack()
     root.mainloop()
 
+
+def track(x,y):
+    alarms=[]
+    count=[]
+    temp=0
+    setalarms=0
+    f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+    f.write("entry-time", localtime())
+    f.close()
+
+
+    if(x=='U'):
+       if y in alarms:
+           index1=alarms.index(y)
+           temp=count.pop(index1)
+           temp=temp+1
+           count.insert(index1,temp)
+           if(temp>3):
+               setsetalarms=1
+               f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+               f.write("name-",y,"Tried to connect more than 3 times","time",localtime() )
+               f.close()
+               BlinkyDataBaseManagment.closeSQLconnection();
+       else:
+           alarms.append(y)
+           count.append(1)
+    elif(x=='DEL-M'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "delete mentor", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    elif(x=='DEL-U'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "delete user", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    elif (x == 'DL_PIC'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "delete pic", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    elif (x == 'UP_PIC'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "upload picture", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    elif (x == 'USER INFO'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "user information", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    elif (x == 'MEDICL_IN'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "medical information", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    elif (x == 'MEDICL_UP'):
+        if y in alarms:
+            index1 = alarms.index(y)
+            temp = count.pop(index1)
+            temp = temp + 1
+            count.insert(index1, temp)
+            if (temp > 3):
+                setsetalarms = 1
+                f = prog_location + "\Reports" + "\\" + "statisticsReport.txt"
+                f.write("name-", y, "medical update", "time", localtime())
+                f.close()
+                BlinkyDataBaseManagment.closeSQLconnection();
+        else:
+            alarms.append(y)
+            count.append(1)
+    else:
+        pass
